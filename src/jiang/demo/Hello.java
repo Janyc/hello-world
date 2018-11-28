157f2b6bb3d2acfee646477c81517eb358c07cbd
Janyc <jiangyongcong@qq.com>
2018-11-28 17:36:04 +0800

    create project

---
+++ src/jiang/demo/Hello.java
@@ -0,0 +1,27 @@
+package jiang.demo;
+
+/**
+ * Author: Jiangyc
+ * Time: 2018-10-18 19:50
+ * Copyright (C) 2018 Xiamen Yaxon Networks CO.,LTD.
+ */
+public class Hello {
+    public static void main( String[] args ) {
+        System.out.print("Hell world!");
+        System.err.println("");
+        System.out.println();
+        System.out.println("Hello.main");
+        System.out.println("args = [" + args + "]");
+        System.out.println("args = " + args);
+        
+        int n=100;
+        for (int i = 0; i < n; i++) {
+            
+        }
+        int[] arr = {1,1,1};
+        for (int i : arr) {
+
+            
+        }
+    }
+}
